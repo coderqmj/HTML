fb5980a6cd5aab309b5bed2654ba7fcb84c3045f
zuogerenba <1732935973@qq.com>
2020-04-28 12:35:41 +0800

    echarts

--- ECharts/js/index.js
+++ ECharts/js/index.js
@@ -1,4 +1,4 @@
-// 成绩柱状图
+// 1.成绩柱状图
 (function () {
   // 1.初始化
   var myChart = echarts.init(document.querySelector(".bar .chart"));
@@ -78,7 +78,7 @@
     myChart.resize();
   });
 })();
-// 学习进度柱状图
+// 2.学习进度柱状图
 (function () {
   var myColor = ['#3498db', '#e74c3c', '#f1c40f', '#2ecc71', '#e056fd'];
   var myChart = echarts.init(document.querySelector('.bar2 .chart'))
@@ -178,4 +178,281 @@
   window.addEventListener("resize", function () {
     myChart.resize();
   });
+})();
+// 3.折线图1的制作
+(function () {
+  var yearData = [
+    {
+      year: '2020',  // 年份
+      data: [  // 两个数组是因为有两条线
+        [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+        [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79]
+      ]
+    },
+    {
+      year: '2021',  // 年份
+      data: [  // 两个数组是因为有两条线
+        [123, 175, 112, 197, 121, 67, 98, 21, 43, 64, 76, 38],
+        [143, 131, 165, 123, 178, 21, 82, 64, 43, 60, 19, 34]
+      ]
+    }
+  ];
+  var myChart = echarts.init(document.querySelector('.line .chart'));
+  option = {
+    color: ['#e74c3c', '#f1c40f'],
+    tooltip: {
+      trigger: 'axis'
+    },
+    legend: {
+      // data: ['邮件营销', '联盟广告'],
+      textStyle: {
+        color: '#4c9bfd'
+      },
+      right: '10%'
+    },
+    grid: {
+      top: '20%',
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      show: true,// 显示边框
+      borderColor: '#012f4a',// 边框颜色
+      containLabel: true // 包含刻度文字在内
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {}
+      }
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      axisTick: {
+        show: false
+      },
+      axisLabel: {
+        textStyle: {
+          color: '#4c9bfd',
+          fontSize: 10
+        }
+      },
+      axisLine: {
+        show: false
+      }
+    },
+    yAxis: {
+      type: 'value',
+      axisTick: {
+        show: false
+      },
+      axisLabel: {
+        textStyle: {
+          color: '#4c9bfd',
+          fontSize: 12
+        }
+      },
+      axisLine: {
+        show: false
+      },
+      splitLine: {
+        show: false
+      }
+    },
+    series: [
+      {
+        name: '新增粉丝',
+        type: 'line',
+        stack: '总量',
+        smooth: true,
+        data: [24, 40, 101, 134, 90, 230, 210, 230, 120, 230, 210, 120],
+      },
+      {
+        name: '新增游客',
+        type: 'line',
+        smooth: true,
+        stack: '总量',
+        data: [40, 64, 191, 324, 290, 330, 310, 213, 180, 200, 180, 79],
+      }
+
+    ]
+  };
+  myChart.setOption(option)
+  // 图标与屏幕自适应
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+  // 点击切换效果
+  $(".line h2").on('click', 'a', function () {
+    // alert(1)
+    // console.log($(this).index())
+    // 点击a之后根据索引号找到对应的yearData相关元素
+    console.log(yearData[$(this).index()])
+    var obj = yearData[$(this).index()]
+    option.series[0].data = obj.data[0]
+    option.series[1].data = obj.data[1]
+    // 数据改变之后需要重新渲染
+    myChart.setOption(option)
+  })
+})();
+// 4.折线图2的制作
+(function () {
+  var myChart = echarts.init(document.querySelector('.line2 .chart'))
+  option = {
+    tooltip: {
+      trigger: 'axis',
+    },
+    legend: {
+      top: "0%",
+      // data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎'],
+      textStyle: {
+        fontSize: 12,
+        color: "rgba(255,255,255,.5)"
+      },
+      // showContent: false
+    },
+    // 图标位置
+    grid: {
+      left: "10",
+      top: "30",
+      right: "10",
+      bottom: "10",
+      containLabel: true
+    },
+    xAxis: [
+      {
+        // show: false,
+        type: 'category',
+        // 文本颜色为rgba(255,255,255,.6)  文字大小为 12
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+        // x轴线的颜色为   rgba(255,255,255,.2)
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.2)"
+          }
+        },
+        boundaryGap: false,
+        data: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "26", "28", "29", "30"]
+      }
+    ],
+    yAxis: [
+      {
+        type: 'value',
+        axisTick: { show: false },
+        axisLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        },
+        axisLabel: {
+          textStyle: {
+            color: "rgba(255,255,255,.6)",
+            fontSize: 12
+          }
+        },
+        // 修改分割线的颜色
+        splitLine: {
+          lineStyle: {
+            color: "rgba(255,255,255,.1)"
+          }
+        }
+      }
+    ],
+    series: [
+      {
+        name: '播放量',
+        smooth: true,
+        type: 'line',
+        // stack: '总量',
+        symbol: "circle",
+        showSymbol: false,
+        symbolSize: 8,
+        itemStyle: {
+          color: "#0184d5",
+          borderColor: "rgba(221, 220, 107, .1)",
+          borderWidth: 12
+        },
+        areaStyle: {
+          // 渐变色，只需要复制即可
+          color: new echarts.graphic.LinearGradient(
+            0,
+            0,
+            0,
+            1,
+            [
+              {
+                offset: 0,
+                color: "rgba(1, 132, 213, 0.4)"   // 渐变色的起始颜色
+              },
+              {
+                offset: 0.8,
+                color: "rgba(1, 132, 213, 0.1)"   // 渐变线的结束颜色
+              }
+            ],
+            false
+          ),
+          shadowColor: "rgba(0, 0, 0, 0.1)"
+        },
+        lineStyle: {
+          color: '#0184d5',
+          width: 3
+        },
+        data: [30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 30, 60, 20, 40, 30, 40, 30, 40, 30, 40, 20, 60, 50, 40]
+      },
+      {
+        name: '转发量',
+        type: 'line',
+        smooth: true,
+        lineStyle: {
+          normal: {
+            color: "#00d887",
+            width: 3
+          }
+        },
+        areaStyle: {
+          normal: {
+            color: new echarts.graphic.LinearGradient(
+              0,
+              0,
+              0,
+              1,
+              [
+                {
+                  offset: 0,
+                  color: "rgba(0, 216, 135, 0.4)"
+                },
+                {
+                  offset: 0.8,
+                  color: "rgba(0, 216, 135, 0.1)"
+                }
+              ],
+              false
+            ),
+            shadowColor: "rgba(0, 0, 0, 0.1)"
+          }
+        },
+        // stack: '总量',
+        areaStyle: {},
+        data: [130, 10, 20, 40, 30, 40, 80, 60, 20, 40, 90, 40, 20, 140, 30, 40, 130, 20, 20, 40, 80, 70, 30, 40, 30, 120, 20, 99, 50, 20],
+        // 设置拐点 小圆点
+        symbol: "circle",
+        // 拐点大小
+        symbolSize: 5,
+        // 设置拐点颜色以及边框
+        itemStyle: {
+          color: "#00d887",
+          borderColor: "rgba(221, 220, 107, .1)",
+          borderWidth: 12
+        },
+        // 开始不显示拐点， 鼠标经过显示
+        showSymbol: false,
+      }
+    ]
+  };
+  myChart.setOption(option)
 })();
