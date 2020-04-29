833dc02cfb10ebefde412d831421c9e399ecf284
zuogerenba <1732935973@qq.com>
2020-04-29 20:44:50 +0800

    echarts

--- ECharts/js/index.js
+++ ECharts/js/index.js
@@ -455,4 +455,106 @@
     ]
   };
   myChart.setOption(option)
+  // 图标与屏幕自适应
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+// 5.饼状图1
+(function () {
+  var myChart = echarts.init(document.querySelector('.pie .chart'));
+  var option = {
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)'
+    },
+    legend: {
+      bottom: '0%',
+      // 1.修改图标大小
+      itemWidth: 10,
+      itemHeight: 10,
+      // 2.修改字体的样式及大小
+      textStyle: {
+        color: 'rgba(255,255,255,.6)',
+        fontSize: 12
+      },
+      data: ['直接访问', '邮件营销', '联盟广告', '视频广告']
+    },
+    series: [
+      {
+        color: [
+          "#065aab",
+          "#066eab",
+          "#0682ab",
+          "#0696ab",
+          "#06a0ab",
+        ],
+        name: '访问来源',
+        type: 'pie',
+        // 1.分别代表饼形图的内圆半径 和 外圆半径
+        radius: ['40%', '60%'],
+        avoidLabelOverlap: false,
+        // 图形上的文字
+        label: {
+          show: false,
+          position: 'center'
+        },
+        // 图形与文字的连线
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 335, name: '直接访问' },
+          { value: 310, name: '邮件营销' },
+          { value: 234, name: '联盟广告' },
+          { value: 135, name: '视频广告' }
+        ]
+      }
+    ]
+  };
+  myChart.setOption(option)
+  // 图标与屏幕自适应
+  window.addEventListener("resize", function () {
+    myChart.resize();
+  });
+})();
+// 6.饼状图2
+(function () {
+  var myChart = echarts.init(document.querySelector('.pie2 .chart'))
+  var option = {
+    color: ['#006cff', '#60cda0', '#ed8884', '#ff9f7f', '#0096ff', '#9fe6b8', '#32c5e9', '#1d9dff'],
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+    series: [
+      {
+        name: '地区分布',
+        type: 'pie',
+        radius: ['10%', '70%'],
+        center: ['50%', '50%'],
+        label: {
+          fontSize: 10
+        },
+        labelLine: {
+          length: 6,
+          length: 10
+        },
+        // 'area'是面积模式，radius是半径模式
+        roseType: 'radius',
+        data: [
+          { value: 20, name: '云南' },
+          { value: 26, name: '北京' },
+          { value: 24, name: '山东' },
+          { value: 25, name: '河北' },
+          { value: 20, name: '江苏' },
+          { value: 25, name: '浙江' },
+          { value: 30, name: '四川' },
+          { value: 42, name: '湖北' }
+        ]
+      }
+    ]
+  };
+  myChart.setOption(option);
+
 })();
